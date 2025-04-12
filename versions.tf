5089695805e972bfad7ad4e7654a32188be10f1e
lzainzinger <lukas.zainzinger@gmail.com>
2025-04-12 12:18:30 +0200

    feat: create sks cluster

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+    backend "s3" {
+        bucket         = "inenpt-state"
+        key            = "terraform.tfstate"
+        region         = "us-east-1"
+    }
+    required_providers {
+        exoscale = {
+            source  = "exoscale/exoscale"
+        }
+    }
+}
+
