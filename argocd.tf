95fdf67792c6781c7b3a55f0c27567777d50bebf
lzainzinger <lukas.zainzinger@gmail.com>
2025-04-12 16:29:39 +0200

    feat: argo lb expose

--- argocd.tf
+++ argocd.tf
@@ -4,12 +4,26 @@ resource "helm_release" "argo_cd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  version          = "5.52.0"
+  version          = "7.8.22"
   timeout          = 1200
   create_namespace = true
   namespace        = "argocd"
   lint             = true
   wait             = true
+
+  values = [
+    yamlencode({
+      server = {
+        service = {
+          type = "LoadBalancer"
+          ports = {
+            http  = 80
+            https = 443
+          }
+        }
+      }
+    })
+  ]
 
   depends_on = [
     local_sensitive_file.kubeconfig_file
