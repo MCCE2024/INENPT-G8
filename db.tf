4f789586c3e60e9439311137ea7527edf39bd63c
chrisfhb <2410781006@hochschule-burgenland.at>
2025-05-24 15:57:19 +0200

    update db.tf

--- db.tf
+++ db.tf
@@ -1,8 +1,8 @@
-resource "exoscale_dbaas" "my_pg" {
-  name = "my-postgres-db"
+resource "exoscale_dbaas" "pg" {
+  name = "postgres-db"
   type = "pg"
   plan = "startup-4"
-  zone = "de-fra-1"
+  zone = var.zone
  
   pg {
     version         = "15"
@@ -10,4 +10,16 @@ resource "exoscale_dbaas" "my_pg" {
     ip_filter       = ["0.0.0.0/0"]
     pg_settings     = jsonencode({ timezone = "Europe/Berlin" })
   }
+}
+ 
+resource "exoscale_dbaas_pg_database" "appdb" {
+  database_name = "appdb"
+  service       = exoscale_dbaas.my_pg.name
+  zone          = exoscale_dbaas.my_pg.zone
+}
+ 
+resource "exoscale_dbaas_pg_user" "appuser" {
+  username = "appuser"
+  service  = exoscale_dbaas.my_pg.name
+  zone     = exoscale_dbaas.my_pg.zone
 }
