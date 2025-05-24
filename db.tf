0253797909542e762417b15279cf0e45a9615de5
chrisfhb <2410781006@hochschule-burgenland.at>
2025-05-24 15:55:38 +0200

    update db.tf

--- db.tf
+++ db.tf
@@ -1,25 +1,13 @@
-resource "exoscale_dbaas_pg" "pg" {
-  name             = "inenpt-g8-db"         # Name des DBaaS-Services
-  plan             = "starter-2"            # Tarif/Größe der Instanz
-  maintenance_day  = "monday"
-  maintenance_time = "10:00:00"
-  termination_protection = false
-  zone             = "at-vie-2"
-
-  pg_user_config {
-    public_access = true
+resource "exoscale_dbaas" "my_pg" {
+  name = "my-postgres-db"
+  type = "pg"
+  plan = "startup-4"
+  zone = "de-fra-1"
+ 
+  pg {
+    version         = "15"
+    backup_schedule = "04:00"
+    ip_filter       = ["0.0.0.0/0"]
+    pg_settings     = jsonencode({ timezone = "Europe/Berlin" })
   }
-}
-
-resource "exoscale_dbaas_pg_database" "appdb" {
-  service = exoscale_dbaas_pg.pg.id
-  name    = "appdb"
-}
-
-resource "exoscale_dbaas_pg_user" "appuser" {
-  service    = exoscale_dbaas_pg.pg.id
-  name       = "appuser"
-  password   = var.pgdb_pw
-  privileges = ["appdb:ALL"]
-  zone       = "at-vie-2"
 }
