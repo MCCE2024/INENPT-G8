b638869e1c4aa1e4918330adb306805dd569d445
Lukas Zainzinger <lukas.zainzinger@gmail.com>
2025-05-31 09:58:19 +0200

    fix: turn off termination policy for db & tofu fmt (#11)

--- versions.tf
+++ versions.tf
@@ -1,20 +1,20 @@
 # Define where to store the state file (S3 remote backend)
 terraform {
   backend "s3" {
-    bucket = "inenpt-state"         # S3 bucket to store the .tfstate file
-    key    = "terraform.tfstate"    # Path to state file within bucket
-    region = "us-east-1"            # AWS region for S3
+    bucket = "inenpt-state"      # S3 bucket to store the .tfstate file
+    key    = "terraform.tfstate" # Path to state file within bucket
+    region = "us-east-1"         # AWS region for S3
   }
 
   # Declare required providers and versions
   required_providers {
     exoscale = {
-      source = "exoscale/exoscale"   # Exoscale cloud provider
+      source  = "exoscale/exoscale" # Exoscale cloud provider
       version = "0.64.1"
     }
     helm = {
-      source  = "hashicorp/helm"     # Helm chart provider
-      version = "2.12.1"             # Specific version
+      source  = "hashicorp/helm" # Helm chart provider
+      version = "2.12.1"         # Specific version
     }
   }
 }
