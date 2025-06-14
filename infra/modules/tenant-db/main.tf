1d5a43491e0351503ad90384f6eeb54baea9844b
Lukas Zainzinger <lukas.zainzinger@gmail.com>
2025-06-14 21:59:49 +0200

    feature/tenant db automation (#15)
    * feat: added module for tenant db creation
    
    * chore: pipeline changes for new directory structure
    
    * fix: pipelines
    
    * fix: module unique name

---
+++ infra/modules/tenant-db/main.tf
@@ -0,0 +1,58 @@
+# Creates the PostgreSQL user with the specified username and the generated password.
+resource "exoscale_dbaas_pg_user" "db_user" {
+  username = var.database_user
+  service  = var.dbaas_service_name
+  zone     = var.dbaas_service_zone
+}
+
+# Creates the PostgreSQL database itself.
+# It depends on the user being created first.
+resource "exoscale_dbaas_pg_database" "db" {
+  database_name = var.database_name
+  service       = var.dbaas_service_name
+  zone          = var.dbaas_service_zone
+  depends_on = [
+    exoscale_dbaas_pg_user.db_user
+  ]
+}
+
+# Fetches the connection URI for the newly created database and user.
+# This is a convenient way to get the full connection string without building it manually.
+data "exoscale_database_uri" "db_uri" {
+  name = var.dbaas_service_name
+  type = "pg"
+  zone = var.dbaas_service_zone
+  
+  # Ensure this data source runs after the database is created.
+  depends_on = [
+    exoscale_dbaas_pg_database.db
+  ]
+}
+
+# Creates the Kubernetes secret with the database credentials.
+resource "kubernetes_secret" "db_secret" {
+  metadata {
+    name      = var.database_name
+    namespace = var.kubernetes_namespace
+  }
+
+  # The data map contains the sensitive information.
+  # It's automatically base64 encoded by Terraform.
+  data = {
+    "DB_HOST"     = data.exoscale_database_uri.db_uri.host
+    "DB_PORT"     = data.exoscale_database_uri.db_uri.port
+    "DB_NAME"     = var.database_name
+    "DB_USER"     = var.database_user
+    "DB_PASSWORD" = exoscale_dbaas_pg_user.db_user.password # The password is fetched from the user resource
+    "DB_URI"      = data.exoscale_database_uri.db_uri.uri # Full connection string
+  }
+
+  type = "Opaque"
+
+  # Ensures the secret is only created after the database and user exist.
+  depends_on = [
+    exoscale_dbaas_pg_database.db,
+    exoscale_dbaas_pg_user.db_user,
+    data.exoscale_database_uri.db_uri,
+  ]
+}
---
+++ infra/modules/tenant-db/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  # Declare required providers and versions
+  required_providers {
+    exoscale = {
+      source  = "exoscale/exoscale" # Exoscale cloud provider
+      version = "0.64.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes" # Kubernetes provider
+      version = "2.37.1"               # Specific version
+    }
+  }
+}
